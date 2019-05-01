338be72cf9b8c055526a6b11a7a27663228252f0
SamG <sam.gabrail@sysdig.com>
2019-05-01 15:32:05 -0400

    Fixed some errors

--- sysdig-agents-install.sh
+++ sysdig-agents-install.sh
@@ -1,25 +1,25 @@
 #Update the access_key below
 access_key=759c8d66-dd32-499a-aaa9-a71bfe6dd90d
 
-msg "*******************Creating namespace sysdig-agents*******************"
+echo "*******************Creating namespace sysdig-agents*******************"
 kubectl create namespace sysdig-agents
 
-msg "*******************creating secret for access_key*******************"
+echo "*******************creating secret for access_key*******************"
 kubectl create secret generic sysdig-agent --from-literal=access-key=$access_key -n sysdig-agents
 
-msg "*******************creating clusterrole*******************"
-kubectl apply -f agents/sysdig-agent-clusterrole.yaml -n sysdig-agents
+echo "*******************creating clusterrole*******************"
+kubectl apply -f sysdig-agent-clusterrole.yaml -n sysdig-agents
 
-msg "*******************creating service account*******************"
+echo "*******************creating service account*******************"
 kubectl create serviceaccount sysdig-agent -n sysdig-agents
 
-msg "*******************Creating clusterrole binding*******************"
+echo "*******************Creating clusterrole binding*******************"
 kubectl create clusterrolebinding sysdig-agent --clusterrole=sysdig-agent --serviceaccount=sysdig-agents:sysdig-agent
 
-msg "*******************Deploying Agent Config*******************"
-kubectl apply -f agents/sysdig-agent-configmap.yaml -n sysdig-agents
+echo "*******************Deploying Agent Config*******************"
+kubectl apply -f sysdig-agent-configmap.yaml -n sysdig-agents
 
-msg "*******************Deploying Agents*******************"
-kubectl apply -f agents/sysdig-agent-daemonset-v2.yaml -n sysdig-agents
+echo "*******************Deploying Agents*******************"
+kubectl apply -f sysdig-agent-daemonset-v2.yaml -n sysdig-agents
 
-msg "*******************It will take about two minutes for the agents to come up.  HAPPY HACKING*******************"
+echo "*******************It will take about two minutes for the agents to come up.  HAPPY HACKING*******************"
